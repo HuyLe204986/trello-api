1430c3dfe649fb32d6d0a15311d537aab8adef4b
huy.ltn204986 <huy.ltn204986@sis.hust.edu.vn>
2024-06-27 11:19:34 +0700

    create API

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,4 +1,5 @@
 import { slugify } from '~/utils/formatters'
+import { boardModel } from '~/models/boardModel'
 const createNew = async (reqBody) => {
   // eslint-disable-next-line no-useless-catch
   try {
@@ -8,7 +9,14 @@ const createNew = async (reqBody) => {
       slug: slugify(reqBody.title)
     }
 
-    return newBoard
+    // gọi tới model để xử lí lưu data và DB
+    const createdBoard = await boardModel.createNew(newBoard)
+    console.log(createdBoard)
+
+    // Lấy bản ghi board sau khi tạo
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
+    console.log(getNewBoard)
+    return getNewBoard
   } catch (error) {
     throw error
   }
